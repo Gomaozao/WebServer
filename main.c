24506d75d566b307da50848cd7cf1e5f92ec6e3b
Gomaozao <40327219+Gomaozao@users.noreply.github.com>
2026-09-20 19:12:32 -0300

    set serverSocket, serverAddress and bind socket to address

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PORT 9090
+#define BUFFER_SIZE 1024
+
+int main ()
+{
+
+    int serverSock;
+
+    if (( serverSock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("could not get our server socket fd");
+        return -1;
+    }
+
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+
+    if (bind(serverSock, (struct sockaddr *)&serverAddr, sizeof serverAddr) < 0)
+    {
+        perror("could not bind to server socket and address");
+        return -1;
+    }
+
+    if (listen(serverSock, 5) < 0)
+    {
+        perror("could not listen on server socket and ip");
+        return -1;
+    }
+
+    printf("listening on port %d\n", PORT);
+
+    while (1)
+    {
+        struct sockaddr_in clientAddr;
+        socklen_t clientLen = sizeof clientAddr;
+        int *clientSocket = malloc(sizeof(int));
+    }
+
+    return 0;
+}
